041bd906bfe1fd0a14e054131a34ca340e4a63c0
RiPires-nucria <fc49807@alunos.fc.ul.pt>
2022-11-24 15:37:37 +0000

    plot data

--- RBS_23Novembro/PlotData.py
+++ RBS_23Novembro/PlotData.py
@@ -6,7 +6,7 @@ import csv
 #############################################
 ###   Plot RBS .dat funntion definition   ###
 #############################################
-def VdG_Plot(File):
+def VdG_Plot(File, dataLabel):
     """
     Converts .dat files from VdG RBS into yield and channel lists
     INPUTS:
@@ -31,12 +31,13 @@ def VdG_Plot(File):
             y.append(float(aux[i][k]))
     
     fig, ax = plt.subplots()
-    ax.plot(ch,y,'.', color ='xkcd:black', label=(str(File)))
-    #ax.semilogy(ch,y,'*', color ='xkcd:purple', label=(str(File)))
+    ax.plot(ch,y,'.', color ='xkcd:black', label=(str(dataLabel)))
+    #ax.semilogy(ch,y,'.', color ='xkcd:purple', label=(str(File)))
     legend = ax.legend(loc="upper right",ncol=2, shadow=False,fancybox=True,framealpha = 0.0,fontsize=20)
     legend.get_frame().set_facecolor('#DAEBF2')
     tick_params(axis='both', which='major', labelsize=22)
-    xlabel('Energy (keV)',fontsize=22)
+    #xlabel('Energy (keV)',fontsize=22)
+    xlabel('Channel',fontsize=22)   
     ylabel('Yield', fontsize=22)
     grid()
     show()
@@ -68,7 +69,7 @@ def VdG_PlotBoth(File1, File2):
     #print(aux)
     for i in range(len(aux)):
         for k in range(8):
-            ch1.append((int(8*(i)+k+1))*1.8511+69.458) ## axes in keV
+            ch1.append((int(8*(i)+k+1))*2.3681+94.322) ## axes in keV
             y1.append(float(aux[i][k]))
 
     ## Creates channel yield lists for File2 and
@@ -84,14 +85,14 @@ def VdG_PlotBoth(File1, File2):
     #print(aux)
     for i in range(len(aux)):
         for k in range(8):
-            ch2.append((int(8*(i)+k+1))*1.8511+69.458) ## axes in keV
+            ch2.append((int(8*(i)+k+1))*2.3681+94.322) ## axes in keV
             y2.append(float(aux[i][k]))
     
     fig, ax = plt.subplots()
-    ax.plot(ch1,y1,'.', color ='xkcd:blue', label=(str(File1)))
-    ax.plot(ch2,y2,'+', color ='xkcd:red', label=(str(File2)))
-    #ax.semilogy(ch1,y1,'.', color ='xkcd:blue', label=(str(File1)))
-    #ax.semilogy(ch2,y2,'+', color ='xkcd:red', label=(str(File2)))
+    #ax.plot(ch1,y1,'.', color ='xkcd:blue', label=(str(File1)))
+    #ax.plot(ch2,y2,'+', color ='xkcd:red', label=(str(File2)))
+    ax.semilogy(ch1,y1,'.', color ='xkcd:blue', label=(str(File1)))
+    ax.semilogy(ch2,y2,'+', color ='xkcd:red', label=(str(File2)))
     legend = ax.legend(loc="upper right",ncol=1, shadow=False,fancybox=True,framealpha = 0.0,fontsize=20)
     legend.get_frame().set_facecolor('#DAEBF2')
     tick_params(axis='both', which='major', labelsize=22)
@@ -103,14 +104,36 @@ def VdG_PlotBoth(File1, File2):
 #############################################
 
 ###                 Plots                     ###
-#VdG_Plot('20220907/ERDrun01.dat') # V-Ta-Nb    
-VdG_Plot('20220907/ERDrun03.dat') # Yb-Ge-Si-O  
+
+"""
+###############   ALFAS   ###############
+VdG_Plot('1122/alfas/RBS1run_1.dat', 'Calib Ta-V-Nb') # Calibration V-Ta-Nb    
+
+VdG_Plot('1122/alfas/RBS1run2.dat', 'target 1 - 21 mm') ## Target 1 - large hole
+VdG_Plot('1122/alfas/RBS1run3.dat', 'target 1 - 23 mm') ## Target 1 - large hole
+VdG_Plot('1122/alfas/RBS1run4.dat', 'target 1 - 25 mm') ## Target 1 - large hole
+VdG_Plot('1122/alfas/RBS1run5.dat', 'target 1 - 27 mm') ## Target 1 - large hole
+
+VdG_Plot('1122/alfas/RBS1run8.dat', 'target 2 - 44 mm') ## Target 2 - formvar
+VdG_Plot('1122/alfas/RBS1run09.dat', 'target 2 - 46 mm') ## Target 2 - formvar
+
+VdG_Plot('1122/alfas/RBS1run10.dat', 'target 3 - 55 mm') ## Target 3 - small hole
+VdG_Plot('1122/alfas/RBS1run11.dat', 'target 3 - 57 mm') ## Target 3 - small hole
+
+VdG_Plot('1122/alfas/RBS1run12.dat', 'target 4 - 66 mm') ## Target 4 - small hole
+VdG_Plot('1122/alfas/RBS1run14.dat', 'target 4 - 65 mm') ## Target 4 - small hole
+
 #################################################
-#VdG_PlotBoth('20220907/RBS1run01.dat', '20220907/RBS1run02.dat')
-#VdG_PlotBoth('20220907/RBS2run01.dat', '20220907/RBS2run02.dat')
-#VdG_PlotBoth('20220907/ERDrun01.dat', '20220907/ERDrun02.dat')
-#################################################
-VdG_PlotBoth('20220907/RBS1run04.dat', '20220907/RBS1run05.dat')
-VdG_PlotBoth('20220907/RBS2run04.dat', '20220907/RBS2run05.dat')
-VdG_PlotBoth('20220907/ERDrun04.dat', '20220907/ERDrun05.dat')
-#################################################
+VdG_PlotBoth('1122/alfas/RBS1run10.dat', '1122/alfas/RBS1run11.dat')
+VdG_PlotBoth('1122/alfas/ERDrun10.dat', '1122/alfas/ERDrun11.dat')
+VdG_PlotBoth('1122/alfas/RBS2run10.dat', '1122/alfas/RBS2run11.dat')
+"""
+
+###############   PROTOES   ###############
+VdG_Plot('1122/protoes/RBS1run16.dat', 'Calib Ta-V-Nb') # Calibration V-Ta-Nb    
+
+VdG_Plot('1122/protoes/RBS1run_17.dat', 'target 1 - 21 mm') ## Target 1 - large hole
+VdG_Plot('1122/protoes/RBS1run18.dat', 'target 1 - 23 mm') ## Target 1 - large hole
+VdG_Plot('1122/protoes/RBS1run19.dat', 'target 1 - 21 mm') ## Target 1 - large hole
+VdG_Plot('1122/protoes/RBS1run20.dat', 'target 1 - 25 mm') ## Target 1 - large hole
+VdG_Plot('1122/protoes/RBS1run21.dat', 'target 1 - 27 mm') ## Target 1 - large hole
